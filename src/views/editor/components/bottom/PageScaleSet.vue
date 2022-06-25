de5a21409b0ea3f705abff9dfb391693d79d433d
“monstereat  git config --global user.email ”532160108@qq.com <532160108@qq.com>
2022-06-25 19:47:25 +0800

    feat: 底部导缩放布局及鼠标缩放初版功能。

---
+++ src/views/editor/components/bottom/PageScaleSet.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="pageScale-menu">
+    <div class="pageScale-menu_adjust pageScale-menu_item">调整尺寸</div>
+    <div class="pageScale-menu_sub pageScale-menu_item">-</div>
+    <div class="pageScale-menu_value pageScale-menu_item">56%</div>
+    <div class="pageScale-menu_add pageScale-menu_item">+</div>
+    <div class="pageScale-menu_lineSwitch pageScale-menu_item" @click="scaleMenu.showSwitch = true">
+      <div class="lineSwitch-wrapper" v-show="scaleMenu.showSwitch">
+        <div class="triangle"></div>
+        <div class="lineSwitch-item" @click.stop="lineSwitch('row')">横向参考线</div>
+        <div class="lineSwitch-item" @click.stop="lineSwitch('col')">竖向参考线</div>
+        <div class="lineSwitch-item" @click.stop="lineSwitch('clear')">清除参考线</div>
+      </div>
+      参考线
+    </div>
+    <div></div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { reactive } from 'vue'
+
+const scaleMenu = reactive({
+  showSwitch: false,
+  lineType: ''
+})
+const lineSwitch = (type: string) => {
+  scaleMenu.showSwitch = false
+  scaleMenu.lineType = type
+}
+</script>
+
+<style lang="scss" scoped>
+.pageScale-menu {
+  bottom: 0;
+  width: 338px;
+  background: rgba(0, 0, 0, 0.8);
+  border-radius: 20px;
+  bottom: 16px;
+  box-sizing: border-box;
+  height: 38px;
+  left: 50%;
+  padding: 0 20px;
+  position: absolute;
+  transform: translate(-50%);
+  z-index: 10;
+  font-size: 14px;
+  color: white;
+  display: flex;
+  user-select: none;
+  .pageScale-menu_adjust {
+    width: 68px;
+  }
+  .pageScale-menu_sub {
+    width: 32px;
+    margin-left: 10px;
+  }
+  .pageScale-menu_value {
+    width: 64px;
+  }
+  .pageScale-menu_add {
+    width: 32px;
+    margin-right: 10px;
+  }
+  .pageScale-menu_lineSwitch {
+    width: 82px;
+    position: relative;
+    .lineSwitch-wrapper {
+      background-color: rgba(0, 0, 0, 0.8);
+      width: 100px;
+      border-radius: 6px;
+      bottom: 44px;
+      left: 50%;
+      padding: 12px 0;
+      position: absolute;
+      transform: translateX(-50%);
+    }
+    .triangle {
+      border: 5px solid transparent;
+      border-top-color: rgba(0, 0, 0, 0.8);
+      bottom: -10px;
+      height: 0;
+      left: 50%;
+      position: absolute;
+      transform: translateX(-50%);
+      width: 0;
+    }
+    .lineSwitch-item {
+      box-sizing: border-box;
+      height: 27px;
+      line-height: 27px;
+      opacity: 0.8;
+      //   padding: 0 20px;
+      position: relative;
+      text-align: center;
+      width: 100%;
+      &:hover {
+        background-color: #000;
+      }
+    }
+  }
+  .pageScale-menu_item {
+    line-height: 38px;
+    height: 38px;
+    text-align: center;
+    cursor: pointer;
+    &:hover {
+      background-color: #000;
+    }
+  }
+  .btn-hover {
+    background-color: #000;
+  }
+}
+</style>
---
+++ src/views/editor/components/bottom/pageScale.ts
@@ -0,0 +1,91 @@
+import { onUnmounted, onMounted, reactive, ref, Ref } from 'vue'
+
+declare type Nullable<T> = T | null
+declare type ElRef<T extends HTMLElement = HTMLDivElement> = Nullable<T>
+const pageScale = () => {
+  const previewRef: Ref<ElRef> = ref(null)
+  const canvasRef: Ref<ElRef> = ref(null)
+  const previewStyle = reactive({
+    transform: 'matrix(1,0,0,1,0,0)',
+    transformOrigin: '0px 0px',
+    preWidth: 0,
+    preHeight: 0
+  })
+
+  const canvasOriginSize = reactive({
+    width: 1080,
+    height: 1920
+  })
+
+  const canvas = reactive({
+    width: 0,
+    height: 0,
+    scaleRate: 0,
+    offsetX: 0,
+    offsetY: 0
+  })
+  /**
+   * 逻辑 画布容器  短边 - 140 计算出画布大小。
+   *  根据大小算缩放比例/  算缩x/y放偏移量
+   */
+  const setOriginScale = () => {
+    // 画布容器实际大小
+    previewStyle.preWidth = previewRef.value!.clientWidth
+    previewStyle.preHeight = previewRef.value!.clientHeight
+    // 比例 用于比较长短边
+    const preWidthRate = previewStyle.preWidth / canvasOriginSize.width
+    const preHeightRate = previewStyle.preHeight / canvasOriginSize.height
+
+    if (preWidthRate <= preHeightRate) {
+      canvas.scaleRate = (previewStyle.preWidth - 140) / canvasOriginSize.width
+    } else {
+      canvas.scaleRate = (previewStyle.preHeight - 140) / canvasOriginSize.height
+    }
+
+    // x y 轴偏移
+    canvas.offsetX = (previewStyle.preWidth - canvasOriginSize.width * canvas.scaleRate) / 2
+    canvas.offsetY = (previewStyle.preHeight - canvasOriginSize.height * canvas.scaleRate) / 2
+
+    previewStyle.transform = `matrix(${canvas.scaleRate},0,0,${canvas.scaleRate},${canvas.offsetX}, ${canvas.offsetY})`
+  }
+
+  const setWheelScale = (ev: any) => {
+    let down = true
+    down = ev.wheelDelta ? ev.wheelDelta < 0 : ev.detail > 0
+    if (down) {
+      // 向下
+      canvas.scaleRate += 0.03
+      canvas.offsetX = (previewStyle.preWidth - canvasOriginSize.width * canvas.scaleRate) / 2
+      canvas.offsetY = (previewStyle.preHeight - canvasOriginSize.height * canvas.scaleRate) / 2
+      previewStyle.transform = `matrix(${canvas.scaleRate},0,0,${canvas.scaleRate},${canvas.offsetX}, ${canvas.offsetY})`
+    } else {
+      if (canvas.scaleRate === 0.1) {
+        return
+      }
+      canvas.scaleRate -= 0.03
+      canvas.offsetX = (previewStyle.preWidth - canvasOriginSize.width * canvas.scaleRate) / 2
+      canvas.offsetY = (previewStyle.preHeight - canvasOriginSize.height * canvas.scaleRate) / 2
+      previewStyle.transform = `matrix(${canvas.scaleRate},0,0,${canvas.scaleRate},${canvas.offsetX}, ${canvas.offsetY})`
+    }
+    if (ev.preventDefault) {
+      ev.preventDefault()
+    }
+  }
+
+  onMounted(() => {
+    setOriginScale()
+    previewRef.value!.addEventListener('mousewheel', setWheelScale)
+  })
+
+  onUnmounted(() => {
+    previewRef.value!.removeEventListener('mousewheel', setWheelScale)
+  })
+
+  return {
+    previewStyle,
+    previewRef,
+    canvasRef
+  }
+}
+
+export default pageScale

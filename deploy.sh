77077f2934881e9f57b800bcb21fc2f0d43addac
zxl7 <zxlcoder@gamil.com>
2022-07-07 15:46:38 +0800

    feat: tag触发自动部署

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 # sudo chmod -R 777 ./deploy.sh 给予权限
 # 当发生错误时中止脚本
 set -e
-# yarn build
+yarn build
 docker  build  --platform linux/amd64 -t  design-templates:1.0.0  .
 docker tag design-templates:1.0.0  zxlcoder/zxlcoder
 docker push zxlcoder/zxlcoder
